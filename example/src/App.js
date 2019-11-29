79b5045b4bfec60d42fde199f44b97cd2453f44d
deeayeen <dee.aye.en@gmail.com>
2019-11-30 00:21:39 +0200

    added example

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,54 @@ import React, { Component } from "react";
 import MediumProfile from "react-medium-profile";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "dee.aye.en"
+    };
+  }
   render() {
     return (
-      <div>
-        <MediumProfile username={"dee.aye.en"} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "80px"
+        }}
+      >
+        <h2 style={{ marginBottom: "60px" }}>{"<MediumProfile />"}</h2>
+        <MediumProfile username={this.state.username} />
+        <h1 style={{ margin: 0, paddingTop: "15px" }}>👆</h1>
+        <h5
+          style={{
+            textAlign: "center",
+            margin: 0,
+            paddingTop: "5px",
+            marginBottom: "20px"
+          }}
+          onClick={() => this.setState({ username: "treyhuffine" })}
+        >
+          Click the button to fetch my Medium profile
+        </h5>
+        <div
+          style={{
+            borderTop: "0.2px solid lightgray",
+            borderBottom: "0.2px solid lightgray",
+            width: "100%"
+          }}
+        >
+          <h4>props</h4>
+          <div style={{ marginLeft: "15px" }}>
+            <h4>
+              username: string <span style={{ color: "gray" }}>required</span>
+            </h4>
+            <h4>
+              size: number <span style={{ color: "gray" }}>optional</span>
+            </h4>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import MediumLogo from "./medium.png";
 
 import styles from "./styles.css";
 
@@ -11,17 +12,40 @@ export default class MediumProfile extends Component {
       items: undefined
     };
   }
+  static defaultProps = {
+    username: "dee.aye.en",
+    size: 100
+  };
   static propTypes = {
-    username: PropTypes.string
+    username: PropTypes.string,
+    size: PropTypes.number
   };
   componentDidMount() {
     fetch(
       `https://medium-profile-fetch.herokuapp.com/profile/@${this.props.username}`
     )
       .then(result => result.json())
-      .then(data => this.setState({ items: data.items }));
+      .then(data => {
+        console.log(data.feed);
+        var filtered = data.items.filter(item => item.categories.length > 0);
+        console.log(filtered[0]);
+        this.setState({ items: filtered });
+      });
   }
   render() {
-    return <div>{this.state.items && <p>{this.state.items.length}</p>}</div>;
+    console.log(this.props);
+    return (
+      <div
+        style={{
+          width: `${this.props.size}px`,
+          height: `${this.props.size}px`,
+          cursor: "pointer",
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+        }}
+      >
+        <img style={{ width: "100%", height: "100%" }} src={MediumLogo} />
+      </div>
+    );
   }
 }
