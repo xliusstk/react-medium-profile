732e48890abe185802eec370ecb0b858f1a3a6cb
deeayeen <dee.aye.en@gmail.com>
2019-11-29 21:05:13 +0200

    i like where this is going

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-medium-profile'
+import MediumProfile from "react-medium-profile";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <MediumProfile username={"dee.aye.en"} />
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
+import styles from "./styles.css";
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export default class MediumProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: undefined,
+      items: undefined
+    };
   }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+  static propTypes = {
+    username: PropTypes.string
+  };
+  componentDidMount() {
+    fetch(
+      `https://medium-profile-fetch.herokuapp.com/profile/@${this.props.username}`
     )
+      .then(result => result.json())
+      .then(data => this.setState({ items: data.items }));
+  }
+  render() {
+    return <div>{this.state.items && <p>{this.state.items.length}</p>}</div>;
   }
 }
