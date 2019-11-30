b9cdb9f94e58040b495eb36cb1e8eba5bf180f87
deeayeen <dee.aye.en@gmail.com>
2019-11-30 03:24:39 +0200

    done

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-
+import GithubLogo from "./github.jpg";
 import MediumProfile from "react-medium-profile";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "dee.aye.en"
+      username: "dee.aye.en",
+      search: ""
     };
   }
   render() {
@@ -17,23 +18,63 @@ export default class App extends Component {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          padding: "80px"
+          padding: "70px"
         }}
       >
-        <h2 style={{ marginBottom: "60px" }}>{"<MediumProfile />"}</h2>
-        <MediumProfile username={this.state.username} />
-        <h1 style={{ margin: 0, paddingTop: "15px" }}>👆</h1>
+        <h2 style={{}}>{"<MediumProfile />"}</h2>
+        <h5 style={{ color: "gray", marginBottom: "50px", marginTop: 0 }}>
+          npm i react-medium-profile
+        </h5>
+
+        <MediumProfile
+          username={
+            this.state.search.length > 0
+              ? this.state.search
+              : this.state.username
+          }
+        />
+        <h1 style={{ margin: 0, paddingTop: "30px" }}>
+          <span role="img" aria-label="pointing up">
+            👆
+          </span>
+        </h1>
         <h5
           style={{
             textAlign: "center",
             margin: 0,
-            paddingTop: "5px",
-            marginBottom: "20px"
+            paddingTop: "5px"
           }}
           onClick={() => this.setState({ username: "treyhuffine" })}
         >
-          Click the button to fetch my Medium profile
+          Click to fetch Medium profile
         </h5>
+        <h6 style={{ margin: 0, paddingTop: "10px", color: "gray" }}>or</h6>
+        <input
+          style={{
+            marginTop: "10px",
+            border: "none",
+            marginBottom: this.state.search.length > 0 ? 0 : "40px",
+            padding: "3px",
+            textAlign: "center",
+            borderBottom: "1px solid black"
+          }}
+          type="text"
+          value={this.state.search}
+          onChange={e => this.setState({ search: e.target.value })}
+          placeholder="Find a profile"
+        ></input>
+        {this.state.search.length > 0 && (
+          <h6
+            style={{
+              marginTop: 0,
+              marginBottom: "40px",
+              paddingTop: "5px",
+              color: "gray"
+            }}
+          >
+            Now click the Medium button
+          </h6>
+        )}
         <div
           style={{
             borderTop: "0.2px solid lightgray",
@@ -50,6 +91,14 @@ export default class App extends Component {
               size: number <span style={{ color: "gray" }}>optional</span>
             </h4>
           </div>
+        </div>
+        <div style={{ position: "fixed", top: "10px", right: "10px" }}>
+          <a
+            target="_blank"
+            href="https://github.com/deeayeen/react-medium-profile"
+          >
+            <img width="30px" src={GithubLogo} />
+          </a>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import MediumLogo from "./medium.png";
-
-import styles from "./styles.css";
+import Modal, { closeStyle } from "simple-react-modal";
+import FadeIn from "react-fade-in";
 
 export default class MediumProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed: undefined,
-      items: undefined
+      loading: true,
+      title: undefined,
+      image: undefined,
+      items: undefined,
+      error: false,
+      show: false
     };
+    this.open = this.open.bind(this);
   }
   static defaultProps = {
     username: "dee.aye.en",
@@ -20,20 +25,31 @@ export default class MediumProfile extends Component {
     username: PropTypes.string,
     size: PropTypes.number
   };
-  componentDidMount() {
+  open() {
+    this.setState({ show: true, loading: true });
     fetch(
       `https://medium-profile-fetch.herokuapp.com/profile/@${this.props.username}`
     )
       .then(result => result.json())
       .then(data => {
-        console.log(data.feed);
-        var filtered = data.items.filter(item => item.categories.length > 0);
-        console.log(filtered[0]);
-        this.setState({ items: filtered });
+        if (data.status === "ok") {
+          this.setState({
+            items: data.items,
+            title: data.feed.title,
+            image: data.feed.image,
+            loading: false
+          });
+        } else {
+          this.setState({
+            items: [],
+            image: MediumLogo,
+            error: true,
+            loading: false
+          });
+        }
       });
   }
   render() {
-    console.log(this.props);
     return (
       <div
         style={{
@@ -44,7 +60,158 @@ export default class MediumProfile extends Component {
             "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
         }}
       >
-        <img style={{ width: "100%", height: "100%" }} src={MediumLogo} />
+        <img
+          onClick={() => this.open()}
+          style={{ width: "100%", height: "100%" }}
+          src={MediumLogo}
+        />
+        <Modal
+          style={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+          containerStyle={{
+            border: "2px solid black",
+            padding: "15px",
+            width: "70vw",
+            cursor: "auto",
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+          }}
+          closeOnOuterClick={true}
+          show={this.state.show}
+          onClose={() => this.setState({ show: false })}
+        >
+          <a
+            style={{ ...closeStyle, color: "white", backgroundColor: "black" }}
+            onClick={() => this.setState({ show: false })}
+          >
+            X
+          </a>
+          {!this.state.loading ? (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column"
+              }}
+            >
+              <img
+                src={this.state.image}
+                width="80px"
+                style={{
+                  borderRadius: 40,
+                  marginTop: "5px",
+                  marginBottom: "15px",
+                  boxShadow:
+                    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                }}
+              />
+              <div
+                style={{
+                  borderBottom: "1px solid lightgray",
+                  marginBottom: "10px",
+                  paddingBottom: "20px",
+                  display: "flex",
+                  alignItems: "center"
+                }}
+              >
+                {!this.state.error ? (
+                  <h5
+                    style={{
+                      margin: 0,
+                      padding: 0,
+                      fontWeight: "lighter"
+                    }}
+                  >
+                    Latest activity by{" "}
+                    <a
+                      style={{ fontWeight: "bolder", color: "black" }}
+                      href={`https://medium.com/@${this.props.username}`}
+                      target="_blank"
+                    >
+                      {this.state.items.length > 0
+                        ? this.state.items[0].author
+                        : this.props.username}
+                    </a>{" "}
+                    on
+                  </h5>
+                ) : (
+                  <h5
+                    style={{
+                      margin: 0,
+                      padding: 0,
+                      fontWeight: "lighter"
+                    }}
+                  >
+                    User not found
+                  </h5>
+                )}
+                <img
+                  style={{ marginLeft: "5px" }}
+                  src={MediumLogo}
+                  width="20px"
+                />
+              </div>
+              <FadeIn>
+                {this.state.items.map((item, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "10px"
+                      //width: "90%"
+                    }}
+                  >
+                    <img
+                      style={{
+                        //border: "1px solid black",
+                        marginRight: "20px",
+                        boxShadow:
+                          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                      }}
+                      src={
+                        item.content[2] !== "p" ? item.thumbnail : MediumLogo
+                      }
+                      width="80px"
+                    />
+                    <div
+                      style={{
+                        margin: 0,
+                        padding: 0,
+                        color: "gray",
+                        fontWeight: "lighter"
+                      }}
+                    >
+                      <a
+                        style={{ color: "gray" }}
+                        target="_blank"
+                        href={item.link}
+                      >
+                        {item.title}
+                      </a>
+                    </div>
+                  </div>
+                ))}
+              </FadeIn>
+            </div>
+          ) : (
+            <div>
+              <FadeIn>
+                <h3
+                  style={{
+                    margin: 0,
+                    padding: 0,
+                    textAlign: "center",
+                    color: "lightgray",
+                    margin: "100px"
+                  }}
+                >
+                  Fetching {this.props.username.toUpperCase()}...
+                </h3>
+              </FadeIn>
+            </div>
+          )}
+        </Modal>
       </div>
     );
   }
